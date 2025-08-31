714ceed202d665edbdf585d55719a197ed953a1e
bitsalv <bitsalv@proton.me>
2025-08-31 02:34:08 +0200

    fix: remove VuePress temporary files and update .gitignore
    - Remove .vuepress/.temp/ and .vuepress/.cache/ from repository
    - Add proper .gitignore rules for VuePress build files
    - These files should never be committed to version control

--- .vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "/home/user/Documents/edge-mining-docs/docs/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "/home/user/Documents/edge-mining-docs/docs/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
--- .vuepress/.temp/prismjs/config.js
+++
@@ -1,5 +0,0 @@
-import "/home/user/Documents/edge-mining-docs/docs/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
-import "/home/user/Documents/edge-mining-docs/docs/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
-import "/home/user/Documents/edge-mining-docs/docs/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
-import "/home/user/Documents/edge-mining-docs/docs/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
-import "/home/user/Documents/edge-mining-docs/docs/node_modules/@vuepress/highlighter-helper/lib/client/styles/code-block-title.css"
